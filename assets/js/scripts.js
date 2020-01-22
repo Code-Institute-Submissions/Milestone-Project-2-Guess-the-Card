7a34de02f5d043dc3913dcf9ea8f7c81ae3349d4
JPMurdie <jpmurdie@gmail.com>
2020-01-22 22:30:21 +0000

    FEAT: demonstrated JS to store needed values in an array as clues for guessing game.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,37 @@
-var xhr = new XMLHttpRequest();
+const baseURL = "https://api.scryfall.com/cards/random"
+var card1 = []
+var card2 = []
+var card3 = []
+var card4 = []
+var card5 = []
 
-xhr.open("GET", "https://api.scryfall.com/cards/random");
-xhr.send();
+function getData(cb) {
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printData(data) {
+    if (data.flavor_text == undefined) {
+        getData(printData);
+    } else {
+        console.dir(data);
+        document.getElementById("data").innerHTML = data.flavor_text;
+        card1.push(data.flavor_text, data.set_name, data.type_line, data.image_uris.normal);
     }
-};
+}
+
+getData(printData);
+
+console.log(card1);
+
+
+
